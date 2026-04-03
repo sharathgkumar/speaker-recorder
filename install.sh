3da7a43b4746e58cf15500ec247d19990bc7e8f8
Sha-Bha <sharathgkumar@gmail.com>
2026-04-03 17:00:12 +0200

    Adding Support for PulseAudio Along with Owner
    Singed-off by: Sha-Bha

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 sudo echo "Installing pulseaudio-utils and lame"
 sudo apt-get install pulseaudio-utils lame
+# Install pulseaudio if not already installed - Sha-Bha
+sudo apt update && sudo apt install pulseaudio
 
 DIR=/usr/local/bin/record-speakers
 if [ ! -d "$DIR" ]; then
@@ -9,3 +11,4 @@ if [ ! -d "$DIR" ]; then
 fi
 sudo cp record_speakers.py record-spreakers.svg record_speakers.glade /usr/local/bin/record-speakers
 cp record_speakers.desktop ${HOME}/.local/share/applications/
+
